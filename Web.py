a884c0213605e18019c5175a010a19e618e773af
altered204 <altered204@gmail.com>
2022-12-22 19:13:35 +0000

    Remove OS to create file

--- Web.py
+++ Web.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import functions
-import os
-
-if not os.path.exists("todos.txt"):
-    with open("todos.txt", 'w') as file:
-        pass
 
 todos = functions.get_todos()
 
